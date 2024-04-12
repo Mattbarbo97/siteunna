fe3d8aafb5202d8c6e66e9837f7cdd748d9f2fe4
Mattbarbo97 <138821344+Mattbarbo97@users.noreply.github.com>
2024-04-12 11:11:50 -0300

    Subindo as alteraçoes posteriormente

--- src/utils/formatDate.js
+++
@@ -1,7 +0,0 @@
-const formatDate = (data) => {
-    const dia = data.getDate();
-    const mes = data.getMonth() + 1; // Os meses são indexados a partir de 0
-    const ano = data.getFullYear();
-    return `${dia}/${mes}/${ano}`;
-};
-export default formatDate;
